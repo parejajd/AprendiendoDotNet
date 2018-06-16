84e16f58d4feef91ef45b255ee8aa7f654be2eae
Juan David Pareja Soto <parejajd@outlook.com>
2018-06-16 09:31:18 -0500

    Manejo de fechas

---
+++ Source/Clase 2/ManejoDeFechas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManejoDeFechas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
