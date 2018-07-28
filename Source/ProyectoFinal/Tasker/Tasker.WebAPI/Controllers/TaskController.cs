8d25a9faf25ca7405795e1e61c377ac4619768ba
Juan David Pareja Soto <parejajd@outlook.com>
2018-07-28 11:55:36 -0500

    Metodos del WebAPI

--- Source/ProyectoFinal/Tasker/Tasker.WebAPI/Controllers/TaskController.cs
+++ Source/ProyectoFinal/Tasker/Tasker.WebAPI/Controllers/TaskController.cs
@@ -21,26 +21,53 @@ namespace Tasker.WebAPI.Controllers
         }
 
         [HttpGet]
-        [Route("[action]")]
-        public async Task<IActionResult> Get()
+        [Route("[action]/{projectId?}")]
+        public async Task<IActionResult> Tasks(int projectId = 0)
         {
             //Consultar todas las tareas
-            List<MyTask> tareas = _taskRepository.Tasks.Select(x => new MyTask
+            List<MyTask> tareas = new List<MyTask>();
+            if (projectId == 0)
             {
-                MyTaskId = x.MyTaskId,
-                Name = x.Name,
-                Description = x.Description,
-                CreationDate = x.CreationDate,
-                ModificationDate = x.ModificationDate,
-                DueDate = x.DueDate,
-                IsCompleted = x.IsCompleted,
-                CompletedDate = x.CompletedDate,
-                ProjectId = x.ProjectId,
-                Project = new Project { ProjectName = x.Project.ProjectName },
-                CreatedBy = new Person { FirstName = x.CreatedBy.FirstName, LastName = x.CreatedBy.LastName }
-            }).ToList();
+                tareas = _taskRepository.Tasks;
+            }
+            else
+            {
+                tareas = _taskRepository.Tasks
+                    .Where(x => x.ProjectId == projectId).ToList();
+            }
 
-            return Ok(tareas);
+            return Ok(tareas
+                    .Select(x => new MyTask
+                    {
+                        MyTaskId = x.MyTaskId,
+                        Name = x.Name,
+                        Description = x.Description,
+                        CreationDate = x.CreationDate,
+                        ModificationDate = x.ModificationDate,
+                        DueDate = x.DueDate,
+                        IsCompleted = x.IsCompleted,
+                        CompletedDate = x.CompletedDate,
+                        ProjectId = x.ProjectId,
+                        Project = new Project { ProjectId = x.ProjectId, ProjectName = x.Project.ProjectName },
+                        CreatedBy = new Person { FirstName = x.CreatedBy.FirstName, LastName = x.CreatedBy.LastName }
+                    }).ToList());
         }
+
+        [HttpGet]
+        [Route("[action]/{projectId?}")]
+        public async Task<IActionResult> Projects(int projectId = 0)
+        {
+            //Si el projectId es CERO, retonar todos los proyectos
+            //Si no es cero, retornar el que tenga el id especificado
+            List<Project> proyectos = _taskRepository.Projects
+                   .Where(x => projectId == 0 || x.ProjectId == projectId)
+                   .Select(x => new Project
+                   {
+                       ProjectId = x.ProjectId,
+                       ProjectName = x.ProjectName
+                   }).ToList();
+            return Ok(proyectos);
+        }
+
     }
 }
